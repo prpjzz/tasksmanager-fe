783dfa4190af2cb6ad87d3ef2e6bca2706f8e4e2
thuongtoo <thuongchannel7@gmail.com>
2025-05-20 20:49:45 +0700

    Trang task chưa hoàn thành với tìm kiếm, lọc, xóa

--- src/routes/publicRoutes.jsx
+++ src/routes/publicRoutes.jsx
@@ -1,20 +1,25 @@
-import Home from "../pages/Home";
-import Test from "../pages/Test";
-import AddTask from "../pages/AddTask";
-import DefaultLayout from "../layouts/DefaultLayout";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import Tasks from "../pages/Tasks";
-import AddSchedule from "../pages/AddSchedule";
+// src/routes/publicRoutes.jsx
+import Home from '../pages/Home';
+import Test from '../pages/Test';
+import AddTask from '../pages/AddTask';
+import DefaultLayout from '../layouts/DefaultLayout';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Tasks from '../pages/Tasks';
+import AddSchedule from '../pages/AddSchedule';
+import SchedulePage from '../pages/SchedulePage';
+import UncompletedTasksPage from '../pages/UncompletedTasksPage';
 
 const publicRoutes = [
-  { path: "/", component: Home, layout: DefaultLayout, requiresAuth: true },
-  { path: "/add-task", component: AddTask, layout: DefaultLayout, requiresAuth: true },
-  { path: "/add-schedule", component: AddSchedule, layout: DefaultLayout, requiresAuth: true },
-  { path: "/test", component: Test, layout: DefaultLayout, requiresAuth: true },
-  { path: "/tasks", component: Tasks, layout: DefaultLayout, requiresAuth: true },
-  { path: "/login", component: Login, layout: null, requiresAuth: false },
-  { path: "/register", component: Register, layout: null, requiresAuth: false },
+  { path: '/', component: Home, layout: DefaultLayout, requiresAuth: true },
+  { path: '/add-task', component: AddTask, layout: DefaultLayout, requiresAuth: true },
+  { path: '/add-schedule', component: AddSchedule, layout: DefaultLayout, requiresAuth: true },
+  { path: '/test', component: Test, layout: DefaultLayout, requiresAuth: true },
+  { path: '/tasks', component: Tasks, layout: DefaultLayout, requiresAuth: true },
+  { path: '/login', component: Login, layout: null, requiresAuth: false },
+  { path: '/register', component: Register, layout: null, requiresAuth: false },
+  { path: '/schedules', component: SchedulePage, layout: DefaultLayout, requiresAuth: true },
+  { path: '/uncompleted-tasks', component: UncompletedTasksPage, layout: DefaultLayout, requiresAuth: true },
 ];
 
 export default publicRoutes;
